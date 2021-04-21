1201bedb4b543552c98a39c7d1449976ff854dcb
Sara Strong <sara.n.strong@gmail.com>
2021-04-21 18:06:36 -0400

    addCommas written and tested

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,42 @@
-function addCommas() {}
+/**
+ * Write a function called addCommas
+ * which accepts a number
+ * and converts it into a string
+ * formatted with commas added for readability.
+ * E.g., addCommas("123456789") // "123,456,789"
+ */
+
+function addCommas(argNum) {
+  let neg = false;
+  let num;
+  if (argNum[0] === "-") {
+    neg = true;
+    num = argNum.substring(1);
+  } else {
+    num = argNum;
+  }
+  const len = num.length;
+  if (len < 4) {
+    console.log(argNum);
+    return argNum;
+  }
+  const arr = [];
+  for (let i = len - 1; i >= 3; i -= 3) {
+    arr.unshift(num.substring(i - 2, i + 1));
+  }
+
+  const mod = len % 3;
+  if (mod !== 0) {
+    arr.unshift(num.substring(0, mod));
+  } else {
+    arr.unshift(num.substring(0, 3));
+  }
+  let digits = arr.join(",");
+  if (neg === true) {
+    digits = `-${digits}`;
+  }
+  console.log(digits);
+  return digits;
+}
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,24 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+  test("works for positive numbers < 1000", () => {
+    expect(addCommas("1")).toEqual("1");
+    expect(addCommas("10")).toEqual("10");
+    expect(addCommas("100")).toEqual("100");
+  });
+  test("works for negative numbers < 1000", () => {
+    expect(addCommas("-1")).toEqual("-1");
+    expect(addCommas("-10")).toEqual("-10");
+    expect(addCommas("-100")).toEqual("-100");
+  });
+  test("works for positive numbers >= 1000", () => {
+    expect(addCommas("1000")).toEqual("1,000");
+    expect(addCommas("10000")).toEqual("10,000");
+    expect(addCommas("100000")).toEqual("100,000");
+  });
+  test("works for negative numbers >= 1000", () => {
+    expect(addCommas("-1000")).toEqual("-1,000");
+    expect(addCommas("-10000")).toEqual("-10,000");
+    expect(addCommas("-100000")).toEqual("-100,000");
+  });
 });
